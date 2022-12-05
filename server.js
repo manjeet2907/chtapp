c393dabee94c2bd5a7fc70e930bb8437de23e80a
Manjeet Saini <manjeet29saini@gmail.com>
2022-12-05 17:29:20 +0530

    Removing Socketio Connectivity

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import { notFound, errorHandler } from "./middlewares/errorMiddleware.js";
 import userRoutes from "./Routes/userRoutes.js";
 import chatRoutes from "./Routes/chatRoutes.js";
 import messageRoutes from "./Routes/messageRoutes.js";
-import { Server } from "socket.io";
+// import { Server } from "socket.io";
 import path from "path";
 
 // app configs goes here
@@ -70,44 +70,44 @@ app.use(errorHandler);
 //  cors: { origin: "https://talk-instant.web.app" }
 //  cors: { origin: "https://localhost:3000" }
 
-const io = new Server(server, {
-  pingTimeout: 60000,
-  // cors: { origin: "https://talk-instant.web.app" },
-});
+// const io = new Server(server, {
+//   pingTimeout: 60000,
+//   // cors: { origin: "https://talk-instant.web.app" },
+// });
 
-io.on("connection", (socket) => {
-  console.log("connected to socket.io");
+// io.on("connection", (socket) => {
+//   console.log("connected to socket.io");
 
-  socket.on("setup", (userData) => {
-    socket.join(userData._id);
-    socket.emit("connected");
-  });
+//   socket.on("setup", (userData) => {
+//     socket.join(userData._id);
+//     socket.emit("connected");
+//   });
 
-  socket.on("join chat", (room) => {
-    socket.join(room);
-    console.log("User Joined Room: " + room);
-  });
+//   socket.on("join chat", (room) => {
+//     socket.join(room);
+//     console.log("User Joined Room: " + room);
+//   });
 
-  socket.on("typing", (room) => socket.in(room).emit("typing"));
-  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+//   socket.on("typing", (room) => socket.in(room).emit("typing"));
+//   socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
 
-  socket.on("new message", (newMessageRecieved) => {
-    var chat = newMessageRecieved.chat;
+//   socket.on("new message", (newMessageRecieved) => {
+//     var chat = newMessageRecieved.chat;
 
-    if (!chat.users) return console.log("chat.users not defined");
+//     if (!chat.users) return console.log("chat.users not defined");
 
-    chat.users.forEach((user) => {
-      if (user._id == newMessageRecieved.sender._id) return;
+//     chat.users.forEach((user) => {
+//       if (user._id == newMessageRecieved.sender._id) return;
 
-      socket.in(user._id).emit("message recieved", newMessageRecieved);
-    });
-  });
+//       socket.in(user._id).emit("message recieved", newMessageRecieved);
+//     });
+//   });
 
-  socket.off("setup", () => {
-    console.log("USER DISCONNECTED");
-    socket.leave(userData._id);
-  });
-});
+//   socket.off("setup", () => {
+//     console.log("USER DISCONNECTED");
+//     socket.leave(userData._id);
+//   });
+// });
 
 //  "proxy":"http://localhost:5000/",
 // "heroku-postbuild": "NPM_CONFIG_PRODUCTION=false npm install --prefix frontend && npm run build --prefix frontend"
